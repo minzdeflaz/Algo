edaad1eb01092abe84877bd452337d77b6a87cf5
Bach Vu <bv88@drexel.edu>
2022-07-24 22:43:52 -0400

    autocommit

--- findLeavesOfBinaryTree.py
+++ findLeavesOfBinaryTree.py
@@ -8,4 +8,26 @@
 #         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        nodesHeight = []
+        def getNodeHeight(node: TreeNode):
+            if node == None:
+                return -1
+            
+            leftHeight = getNodeHeight(node.left)
+            rightHeight = getNodeHeight(node.right)
+
+            curHeight = max(leftHeight, rightHeight) + 1
+
+            nodesHeight.append((curHeight, node.val))
+
+            return curHeight
         
+        getNodeHeight(root)
+
+        nodesHeight.sort(key=lambda item: item[0])
+        result = []
+        for height, val in nodesHeight:
+            if len(result) - 1 == height:
+                result.append([])
+            result[height].append(val)
+        return result

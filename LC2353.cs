08abff1c20c26327be9c30281e24238e7220f13d
Bach Vu <bv88@drexel.edu>
2024-03-20 22:28:33 -0400

    autocommit

--- LC2353.cs
+++ LC2353.cs
@@ -8,37 +8,28 @@ cuisine dictionary: {cuisine: {food: rating}}
 cuisine top dictionary: {cuisine : max_heap}
 */
 
-public class CusComparer : IComparer<ValueTuple<string, int>> {
-    public int Compare (ValueTuple<string, int> tup1, ValueTuple<string, int> tup2){
-        if (tup1.Item2 == tup2.Item2){
-            return tup1.Item1.CompareTo(tup2.Item1);
-        } else {
-            return tup2.Item2.CompareTo(tup1.Item2);
-        }
-    }
-}
 
 public class FoodRatings {
     private Dictionary<string, int> food_to_ratings = new();
     private Dictionary<string, string> food_to_cuisine = new();
-    private Dictionary<string, PriorityQueue<(string, int), (string, int)>> top_cuisine_map = new();
+    private Dictionary<string, PriorityQueue<(string, int), (int, string)>> top_cuisine_map = new();
     public FoodRatings(string[] foods, string[] cuisines, int[] ratings) {
         for (int i=0; i<foods.Length; i++) {
             food_to_ratings[foods[i]] = ratings[i];
             food_to_cuisine[foods[i]] = cuisines[i];
             
             if (!top_cuisine_map.ContainsKey(cuisines[i])){
-                top_cuisine_map[cuisines[i]] = new(new CusComparer());
+                top_cuisine_map[cuisines[i]] = new();
             }
 
-            top_cuisine_map[cuisines[i]].Enqueue((foods[i], ratings[i]), (foods[i], ratings[i]));
+            top_cuisine_map[cuisines[i]].Enqueue((foods[i], ratings[i]), (-ratings[i], foods[i]));
         }
     }
     
     public void ChangeRating(string food, int newRating) {
         food_to_ratings[food] = newRating;
         var cuisine = food_to_cuisine[food];
-        top_cuisine_map[cuisine].Enqueue((food, newRating), (food, newRating));
+        top_cuisine_map[cuisine].Enqueue((food, newRating), (-newRating, food));
 
     }
     
@@ -53,7 +44,53 @@ public class FoodRatings {
         return top_food;
     }
 }
+#region Use Custom Comparer
+// public class CusComparer : IComparer<ValueTuple<string, int>> {
+//     public int Compare (ValueTuple<string, int> tup1, ValueTuple<string, int> tup2){
+//         if (tup1.Item2 == tup2.Item2){
+//             return tup1.Item1.CompareTo(tup2.Item1);
+//         } else {
+//             return tup2.Item2.CompareTo(tup1.Item2);
+//         }
+//     }
+// }
 
+// public class FoodRatings {
+//     private Dictionary<string, int> food_to_ratings = new();
+//     private Dictionary<string, string> food_to_cuisine = new();
+//     private Dictionary<string, PriorityQueue<(string, int), (string, int)>> top_cuisine_map = new();
+//     public FoodRatings(string[] foods, string[] cuisines, int[] ratings) {
+//         for (int i=0; i<foods.Length; i++) {
+//             food_to_ratings[foods[i]] = ratings[i];
+//             food_to_cuisine[foods[i]] = cuisines[i];
+            
+//             if (!top_cuisine_map.ContainsKey(cuisines[i])){
+//                 top_cuisine_map[cuisines[i]] = new(new CusComparer());
+//             }
+
+//             top_cuisine_map[cuisines[i]].Enqueue((foods[i], ratings[i]), (foods[i], ratings[i]));
+//         }
+//     }
+    
+//     public void ChangeRating(string food, int newRating) {
+//         food_to_ratings[food] = newRating;
+//         var cuisine = food_to_cuisine[food];
+//         top_cuisine_map[cuisine].Enqueue((food, newRating), (food, newRating));
+
+//     }
+    
+//     public string HighestRated(string cuisine) {
+//         (var top_food, var rating) = top_cuisine_map[cuisine].Peek();
+
+//         while (rating != food_to_ratings[top_food]){
+//             top_cuisine_map[cuisine].Dequeue();
+//             (top_food, rating) = top_cuisine_map[cuisine].Peek();
+//         }
+
+//         return top_food;
+//     }
+// }
+#endregion
 /**
  * Your FoodRatings object will be instantiated and called as such:
  * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
